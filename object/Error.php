ac303a1119743fc0d0b76b14c43f80689499eee8
simpleauthority <jacob@algorithmjunkie.com>
2020-03-11 22:16:12 -0700

    Error should set the given response code

--- object/Error.php
+++ object/Error.php
@@ -4,10 +4,19 @@ defined('_XFRM_API') or exit('No direct script access allowed here.');
 require_once('JsonResponse.php');
 
 class Error extends JsonResponse {
+    private $code;
+
     public function __construct($code, $message) {
         parent::__construct(array(
             "code" => $code,
             "message" => $message
         ));
+
+        $this->code = $code;
+    }
+
+    public function __toString() {
+        http_response_code($this->code);
+        return parent::__toString();
     }
 }
