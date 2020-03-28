c8ba68dba3483c6bccf06981f63a25c86d139a31
simpleauthority <jacob@algorithmjunkie.com>
2020-03-27 20:42:32 -0700

    add identities to author response

--- object/Author.php
+++ object/Author.php
@@ -5,12 +5,20 @@ class Author {
     public $id;
     public $username;
     public $resource_count;
+    public $identities;
     public $avatar;
 
     public function __construct($author) {
         $this->id = $author['user_id'];
         $this->username = $author['username'];
         $this->resource_count = $author['resource_count'];
+
+        $this->identities = array();
+        $identityKey = explode(",", $author['identity_key']);
+        $identityVal = explode(",", $author['identity_val']);
+        for ($idx = 0; $idx < count($identityKey) && $idx < count($identityVal); $idx++) {
+            $this->identities[$identityKey[$idx]] = $identityVal[$idx];
+        }
 
         $this->avatar = array(
             'info' => $author['avatar_date'],
